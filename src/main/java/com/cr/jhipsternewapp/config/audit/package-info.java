19aad5e1b600c41663b466d7eeadbcdb05a65f30
NIKOLAG-WIN\nikola.grkavac <nikola,gc@gmail.com>
2018-01-22 16:04:32 +0100

    Initial application generated by JHipster-4.13.3

---
+++ src/main/java/com/cr/jhipsternewapp/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.cr.jhipsternewapp.config.audit;
---
+++ src/main/java/com/cr/jhipsternewapp/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.cr.jhipsternewapp.config;
